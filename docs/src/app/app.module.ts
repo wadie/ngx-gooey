5f916e26654fdac2b64f555e9e37fa62c810846f
Wadie <1206307+wadie@users.noreply.github.com>
2024-06-28 19:56:30 +0200

    Update to Angular 18

--- docs/src/app/app.module.ts
+++ docs/src/app/app.module.ts
@@ -26,55 +26,46 @@ import {
   NgDocTooltipDirective,
 } from '@ng-doc/ui-kit';
 
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Gooey } from 'ngx-gooey';
 import { AppComponent } from './app.component';
 
-@NgModule({
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    RouterModule.forRoot(NG_DOC_ROUTING, {
-      scrollPositionRestoration: 'enabled',
-      anchorScrolling: 'enabled',
-      scrollOffset: [0, 70],
-    }),
-    RouterOutlet,
-    HttpClientModule,
-    NgDocRootComponent,
-    NgDocNavbarComponent,
-    NgDocSidebarComponent,
-    NgDocIconComponent,
-    NgDocMediaQueryDirective,
-    NgDocButtonIconComponent,
-    NgDocThemeToggleComponent,
-    NgDocTooltipDirective,
-    Gooey,
-  ],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent],
-  providers: [
-    provideNgDocContext(),
-    provideNgDocApp({ defaultThemeId: NG_DOC_NIGHT_THEME.id }),
-    provideSearchEngine(NgDocDefaultSearchEngine),
-    providePageSkeleton(NG_DOC_DEFAULT_PAGE_SKELETON),
-    provideMainPageProcessor(NG_DOC_DEFAULT_PAGE_PROCESSORS),
-    provideRouter(
-      [
-        {
-          path: '',
-          pathMatch: 'full',
-          redirectTo: 'home/installation',
-        },
-      ],
-      withInMemoryScrolling({
-        scrollPositionRestoration: 'enabled',
-        anchorScrolling: 'enabled',
-      })
-    ),
-  ],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        BrowserAnimationsModule,
+        RouterModule.forRoot(NG_DOC_ROUTING, {
+            scrollPositionRestoration: 'enabled',
+            anchorScrolling: 'enabled',
+            scrollOffset: [0, 70],
+        }),
+        RouterOutlet,
+        NgDocRootComponent,
+        NgDocNavbarComponent,
+        NgDocSidebarComponent,
+        NgDocIconComponent,
+        NgDocMediaQueryDirective,
+        NgDocButtonIconComponent,
+        NgDocThemeToggleComponent,
+        NgDocTooltipDirective,
+        Gooey], providers: [
+        provideNgDocContext(),
+        provideNgDocApp({ defaultThemeId: NG_DOC_NIGHT_THEME.id }),
+        provideSearchEngine(NgDocDefaultSearchEngine),
+        providePageSkeleton(NG_DOC_DEFAULT_PAGE_SKELETON),
+        provideMainPageProcessor(NG_DOC_DEFAULT_PAGE_PROCESSORS),
+        provideRouter([
+            {
+                path: '',
+                pathMatch: 'full',
+                redirectTo: 'home/installation',
+            },
+        ], withInMemoryScrolling({
+            scrollPositionRestoration: 'enabled',
+            anchorScrolling: 'enabled',
+        })),
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule {}
